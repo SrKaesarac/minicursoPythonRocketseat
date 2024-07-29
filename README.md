d938939b966d7a17bc668828336b866f8247064b
SrKaesarac <aoghast@gmail.com>
2024-07-29 16:16:17 -0300

    added requirements for installation on requirements.txt; added info on readme; started routes logic on app.py; started data models on sqlite

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # minicursoPythonRockeseat
+
+Comandos:
+flask shell 
+db.create_all() - transforma todas as Models em Tables 
+db.session.commit() - session = conexão com o banco; commit = efetiva as mudanças no Banco, sem esse comando as mudanças não serão registradas.
+exit() - encerra o shell
---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy # orm
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+
+db = SQLAlchemy(app)
+
+class Product(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(120), nullable=False)
+  price = db.Column(db.Float, nullable=False)
+  description = db.Column(db.Text, nullable=True)
+
+@app.route('/')
+def hello():
+  return 'Hello World'
+
+if __name__ == "__main__":
+  app.run(debug=True)
